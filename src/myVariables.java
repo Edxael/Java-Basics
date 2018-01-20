2fd1e2caf7edf9205758a7d71f415a978e0ebe51
Edxael <zerphyr.r@gmail.com>
2018-01-19 19:24:37 -0700

    1st commit

---
+++ src/myVariables.java
@@ -0,0 +1,23 @@
+
+public class myVariables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			// String Variable;
+		String name = "Ayumi Hamasaki";
+		name = "My Favorite Singer: " + name;
+		System.out.println(name);
+		
+			// Integers and its Math.
+		int price = 38;
+		int tax = 2;
+		int total = price + tax;
+		
+		System.out.println(total);
+	}
+
+}
